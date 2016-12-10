3d8a7aca9eddc7fc8b991a68a7d8726afb0d01f0
Thomas Daede <daede003@umn.edu>
2016-12-09 19:06:33 -0800

    Add x and y

--- speechmusic.py
+++ speechmusic.py
@@ -8,4 +8,9 @@ filename_queue = tf.train.string_input_producer(["features_trans5/data"])
 reader = tf.TextLineReader()
 key, value = reader.read(filename_queue)
 
-print(tf.decode_csv(value,record_defaults=[[0.0] for x in range(0,28)],field_delim=' '))
+print(tf.decode_csv(value,record_defaults=[[0.0] for x in range(0,27)],field_delim=' '))
+
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 25])
+y_ = tf.placeholder(tf.float32, shape=[None, 2])
