ac219463620a25789029c8bb964f8a6cde3647a7
Thomas Daede <daede003@umn.edu>
2016-12-10 15:16:42 -0800

    It actually works now

--- speechmusic.py
+++ speechmusic.py
@@ -3,20 +3,22 @@
 import tensorflow as tf
 import numpy as np
 
+sess = tf.InteractiveSession()
+
+
 filename_queue = tf.train.string_input_producer(["features_trans5/data"])
+threads = tf.train.start_queue_runners()
+
+# decode_csv treats extra delimiters as extra fields, the training data has extra spaces
+# between input and output. so column 27 and 28 are the actual outputs
 
 reader = tf.TextLineReader()
-key, value = reader.read_up_to(filename_queue,100) # read a batch of 100
+key, value = reader.read_up_to(filename_queue,1000) # read a batch of 100
 csv = tf.decode_csv(value,record_defaults=[[0.0] for x in range(0,29)],field_delim=' ')
 
 training = tf.pack(csv[0:25],axis=1)
-target = tf.pack(csv[25:27],axis=1)
+target = tf.pack(csv[27:28],axis=1)
 
-
-sess = tf.InteractiveSession()
-
-coord = tf.train.Coordinator()
-threads = tf.train.start_queue_runners(coord=coord)
 
 #x = tf.placeholder(tf.float32, shape=[None, 25])
 #y_ = tf.placeholder(tf.float32, shape=[None, 2])
@@ -24,22 +26,26 @@ threads = tf.train.start_queue_runners(coord=coord)
 x = training
 y_ = target
 
-W = tf.Variable(tf.zeros([25,2]))
-b = tf.Variable(tf.zeros([2]))
+#print(target.eval())
+
+W = tf.Variable(tf.zeros([25,1]))
+b = tf.Variable(tf.zeros([1]))
 
 sess.run(tf.global_variables_initializer())
 
 y = tf.matmul(x,W) + b
 
-#we don't want a softmax here but probably simple MSE, send help
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+mse = tf.reduce_mean(tf.abs(tf.subtract(y, y_)))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(mse)
 
 for i in range(1000):
     # run on the entire set, rip your cpu
     train_step.run()
+    #print(y_.eval())
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+correct_prediction = tf.abs(tf.subtract(tf.minimum(tf.maximum(y,-1),1),y_))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(W.eval())
+print(b.eval())
 print(accuracy.eval())
 
