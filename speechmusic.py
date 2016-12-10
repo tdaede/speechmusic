e0c31bbbe9d0b23a38cac675bb2e2154523107d3
Thomas Daede <daede003@umn.edu>
2016-12-09 20:06:42 -0800

    what am I doing

--- speechmusic.py
+++ speechmusic.py
@@ -6,16 +6,23 @@ import numpy as np
 filename_queue = tf.train.string_input_producer(["features_trans5/data"])
 
 reader = tf.TextLineReader()
-key, value = reader.read(filename_queue)
-
+key, value = reader.read_up_to(filename_queue,100)
 csv = tf.decode_csv(value,record_defaults=[[0.0] for x in range(0,27)],field_delim=' ')
-training = csv[0:25]
-target = csv[25:27]
+
+training = tf.pack(csv[0:25],axis=1)
+target = tf.pack(csv[25:27],axis=1)
+
 
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, shape=[None, 25])
-y_ = tf.placeholder(tf.float32, shape=[None, 2])
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(coord=coord)
+
+#x = tf.placeholder(tf.float32, shape=[None, 25])
+#y_ = tf.placeholder(tf.float32, shape=[None, 2])
+
+x = training
+y_ = target
 
 W = tf.Variable(tf.zeros([25,2]))
 b = tf.Variable(tf.zeros([2]))
@@ -28,9 +35,9 @@ y = tf.matmul(x,W) + b
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-#for i in range(1000):
-#  batch = mnist.train.next_batch(100)
-#  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+for i in range(1000):
+    # run on the entire set, rip your cpu
+    train_step.run()
 
 #correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
